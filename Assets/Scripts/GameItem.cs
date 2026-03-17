7bba49f987e3e4c7349e74e20dcc21e70fd1cd72
braydenleung-Git <brayden.leung@gmail.com>
2026-03-17 17:47:09 -0400

    Created GameItem Abstract Class

---
+++ Assets/Scripts/GameItem.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public abstract class GameItem : ScriptableObject, Hintable
+{
+    [Header("Item Info")]
+    [field: SerializeField] public string ItemName { get; set; }
+    [field: SerializeField] [TextArea]  public string Description { get; set; }
+    [field: SerializeField] public Sprite ItemIcon { get; set; }
+    
+    
+    public bool IsVisible { get; set; }
+    public bool IsOutlined { get; set; }
+    public bool IsHinted { get; set; }
+    public Sprite HintIcon { get; set; }
+}
