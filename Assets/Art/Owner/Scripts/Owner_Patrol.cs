7e7e38adc2e155f2428550870181c2d35f2545e4
Anomaly-E-E <arshanshareef@gmail.com>
2026-03-23 15:50:56 -0400

    NPC patrol works

--- Assets/Art/Owner/Scripts/Owner_Patrol.cs
+++ Assets/Art/Owner/Scripts/Owner_Patrol.cs
@@ -4,29 +4,48 @@ using UnityEngine;
 public class NPC_Patrol : MonoBehaviour
 {
     public Vector2[] patrolPoints;
-    private int currentPatrolIndex;
-    public Vector2 target;
     public float speed = 2;
+    public float pauseDuration = 1.5f;
+
+    private bool isPaused;
+    private int currentPatrolIndex;
+    private Vector2 target;
     private Rigidbody2D rb;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        target = patrolPoints[0];
+        anim = GetComponent<Animator>(); 
+        StartCoroutine(SetPatrolPoint());
     }
 
     void Update()
     {
+        if (isPaused)
+        {
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+
         Vector2 direction = ((Vector3)target - transform.position).normalized;
+        if (direction.x < 0 && transform.localScale.x > 0 || direction.x > 0 && transform.localScale.x < 0)
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.x);
+        
         rb.linearVelocity = direction * speed;
 
         if (Vector2.Distance(transform.position, target) < .1f)
-            SetPatrolPoint();
+            StartCoroutine(SetPatrolPoint());
     }
 
-    private void SetPatrolPoint()
+IEnumerator SetPatrolPoint()
     {
+        isPaused = true;
+        anim.Play("Idle");
+        yield return new WaitForSeconds(pauseDuration);
         currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
         target = patrolPoints[currentPatrolIndex];
+        isPaused = false;
+        anim.Play("Owner_WalkRight");
     }
 }
