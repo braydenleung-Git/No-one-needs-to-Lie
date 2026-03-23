9aa0c4bf5e72f3e67e89a1265587299dceacf69f
Anomaly-E-E <arshanshareef@gmail.com>
2026-03-23 14:59:29 -0400

    Restarting NPC moment

---
+++ Assets/Art/Owner/Scripts/Owner_Patrol.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class Owner_Patrol : MonoBehaviour
+
+{ 
+    public Vector2[] patrolpoints;
+    public float speed = 2f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
