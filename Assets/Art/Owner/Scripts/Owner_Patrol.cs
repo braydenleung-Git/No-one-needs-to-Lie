423ab4d33d13b9230e75316515679e6844b374e8
Anomaly-E-E <arshanshareef@gmail.com>
2026-03-23 15:12:46 -0400

    Fixed code for NPC moment

--- Assets/Art/Owner/Scripts/Owner_Patrol.cs
+++ Assets/Art/Owner/Scripts/Owner_Patrol.cs
@@ -1,20 +1,32 @@
-using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.InputSystem;
+using UnityEngine;
 
-public class Owner_Patrol : MonoBehaviour
+public class NPC_Patrol : MonoBehaviour
+{
+    public Vector2[] patrolPoints;
+    private int currentPatrolIndex;
+    public Vector2 target;
+    public float speed = 2;
+    private Rigidbody2D rb;
 
-{ 
-    public Vector2[] patrolpoints;
-    public float speed = 2f;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        target = patrolPoints[0];
     }
 
     void Update()
     {
-        
+        Vector2 direction = ((Vector3)target - transform.position).normalized;
+        rb.linearVelocity = direction * speed;
+
+        if (Vector2.Distance(transform.position, target) < .1f)
+            SetPatrolPoint();
+    }
+
+    private void SetPatrolPoint()
+    {
+        currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+        target = patrolPoints[currentPatrolIndex];
     }
 }
