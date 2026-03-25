17cc4bfeff5302112062f89a29d1d7d0649db44f
Anomaly-E-E <arshanshareef@gmail.com>
2026-03-25 15:15:41 -0400

    Fixed erros

--- Assets/Scripts/ChestInventory.cs
+++ Assets/Scripts/ChestInventory.cs
@@ -1,43 +1,19 @@
-using System.Collections.Generic;
-using DefaultNamespace;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ChestInventory : MonoBehaviour, Inventory
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        AddItem(new Item())
-    }
+    void Start() { }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
-    public void AddItem(GameItem item)
-    {
-        throw new System.NotImplementedException();
-    }
+    public void AddItem(GameItem item) { }
 
-    public void RemoveItem(GameItem item)
-    {
-        throw new System.NotImplementedException();
-    }
+    public void RemoveItem(GameItem item) { }
 
-    public bool HasItem(GameItem item)
-    {
-        throw new System.NotImplementedException();
-    }
+    public bool HasItem(GameItem item) { return false; }
 
-    public List<GameItem> ListItems()
-    {
-        throw new System.NotImplementedException();
-    }
+    public List<GameItem> ListItems() { return new List<GameItem>(); }
 
-    public void TransferItem(Inventory other, GameItem item)
-    {
-        throw new System.NotImplementedException();
-    }
+    public void TransferItem(Inventory other, GameItem item) { }
 }
