348ead0a779cf42801f2c9bff1b9ccf0b203049d
braydenleung-Git <brayden.leung@gmail.com>
2026-03-26 11:26:17 -0400

    added _transformToPlayer()

--- Assets/Scripts/Cosmetics.cs
+++ Assets/Scripts/Cosmetics.cs
@@ -8,6 +8,7 @@ public class Cosmetics: Item, IEquipable
     {
         _equipPosition = position;
         transform.SetParent(Owner.transform); //set parent of the cosmetics to the Owner(NPC/Player)
+        _transformToPlayer(_equipPosition);
     }
 
     public void Unequip()
@@ -19,6 +20,39 @@ public class Cosmetics: Item, IEquipable
     public EquipPosition GetEquipPosition()
     {
         return (EquipPosition)_equipPosition;
+    }
+
+    /// <summary>
+    /// this method is used to move the cosmetic item relative to the player
+    /// </summary>
+    private void _transformToPlayer(EquipPosition position)
+    {
+        switch(position)
+        {
+            case EquipPosition.None:
+                gameObject.SetActive(false);
+                break;
+            case EquipPosition.Head:
+                gameObject.SetActive(true);
+                transform.localPosition = Vector3.up * 0.2f;
+                break;
+            case EquipPosition.Chest:
+                gameObject.SetActive(true);
+                transform.localPosition = Vector3.up * 0.2f;
+                break;
+            case EquipPosition.Legs:
+                gameObject.SetActive(true);
+                transform.localPosition = Vector3.down * 0.2f;
+                break;
+            case EquipPosition.Feet:
+                gameObject.SetActive(true);
+                transform.localPosition = Vector3.down * 0.2f;
+                break;
+            case EquipPosition.Hands:
+                transform.localPosition = Vector3.zero;
+                break;
+        }
+        
     }
 }
 
